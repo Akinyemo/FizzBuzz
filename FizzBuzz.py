3782636d857a6782199b3adc7943e306a9328eb7
Oyebolaji Akinyemi <akinyemo@flip2.engr.oregonstate.edu>
2021-03-07 20:54:20 -0800

    Implementation for  first test

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,11 @@
 #File containing fizz buzz function
 
 def TestFizzBuzz(x):
-    pass
+    count = 1 
+    for number in range(100):
+        if count % 3 == 0:
+            print('Fizz')
+            if count == x:
+                return 'Fizz'
+        count +=1
+    return '0'
