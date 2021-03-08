5e9093c224650ce2b25333504bea86e3b4fc5d42
Oyebolaji Akinyemi <akinyemo@flip2.engr.oregonstate.edu>
2021-03-07 20:51:12 -0800

    Initial Commit with first test

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,19 +1,4 @@
 #File containing fizz buzz function
 
 def TestFizzBuzz(x):
-    count = 1 
-    for number in range(100):
-        if count % 3 == 0 and count % 5 == 0:
-            print('FizzBuzz')
-            if count == x:
-                return 'FizzBuzz'
-        elif count % 3 == 0:
-            print('Fizz')
-            if count == x:
-                return 'Fizz'
-        elif count % 5 == 0:
-            print('Buzz')
-            if count == x:
-                return 'Buzz'
-        count +=1
-    return '0'
+    pass
--- TestFizzBuzz.py
+++ TestFizzBuzz.py
@@ -5,13 +5,6 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_mult_three(self):
         self.assertEqual(FizzBuzz.TestFizzBuzz(3),"Fizz")
-
-    def test_mult_five(self):
-        self.assertEqual(FizzBuzz.TestFizzBuzz(35),"Buzz")
-
-    def test_mult_five_and_three(self):
-        self.assertEqual(FizzBuzz.TestFizzBuzz(30),"FizzBuzz")
-
         
 if __name__ == "__main__":
     unittest.main()
