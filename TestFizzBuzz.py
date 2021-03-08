f7d168fd3e9c6cede4b460baaf74453d54f757a0
Oyebolaji Akinyemi <akinyemo@flip2.engr.oregonstate.edu>
2021-03-07 20:57:04 -0800

    Added Third Test

--- TestFizzBuzz.py
+++ TestFizzBuzz.py
@@ -9,5 +9,7 @@ class TestFizzBuzz(unittest.TestCase):
     def test_mult_five(self):
         self.assertEqual(FizzBuzz.TestFizzBuzz(35),"Buzz")
         
+    def test_mult_five_and_three(self):
+        self.assertEqual(FizzBuzz.TestFizzBuzz(30),"FizzBuzz")
 if __name__ == "__main__":
     unittest.main()
